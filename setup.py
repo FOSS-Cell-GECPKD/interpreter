30f59a3455de2263ed60abd023e460197502d0d5
AthulJoshi <athuljoshi7@gmail.com>
2020-10-13 16:31:00 +0530

    Converted the project to a package

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="plox", #
+    version="0.0.1",
+    description="An implementation of lox in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FOSS-Cell-GECPKD/interpreter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
